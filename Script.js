b600c8be6f94f7a680f61cb6510f863739d2489d
Fabubakar24 <abubakar.all.dv@gmail.com>
2021-07-07 13:08:45 +0600

    project is done

---
+++ Script.js
@@ -0,0 +1,7 @@
+function videoPlay(){
+    var video = document.querySelector(".image_div img");
+    var icone = document.querySelector(".vdo_section i");
+
+        video.style.display = "none";
+        icone.style.display = "none";
+};
